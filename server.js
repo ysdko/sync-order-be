810f4aee9e6b662626447573793f7e6510d22510
ysdko <koki.yoshida110@gmail.com>
2023-08-11 10:07:34 +0900

    新規注文の名前と個数の同期

--- server.js
+++ server.js
@@ -1,69 +1,62 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const cors = require('cors');
-
-const mongoose = require('mongoose');
-const WebSocket = require('ws');
+const bodyParser = require('body-parser');
 
 const app = express();
-const wss = new WebSocket.Server({ noServer: true });
+const server = http.createServer(app);
+const io = socketIo(server);
 
-app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 
-const orderSchema = new mongoose.Schema({
-  itemName: String,
-  quantity: Number,
-});
+const orders = []; // この例ではメモリ内に注文データを保存しますが、実際のアプリではデータベースを使用するべきです。
 
-const Order = mongoose.model('Order', orderSchema);
+io.on('connection', (socket) => {
+  const roomName = socket.handshake.query.roomName;
 
-// 新しい注文を作成するエンドポイント
-app.post('/orders', async (req, res) => {
-  const newOrder = new Order(req.body);
-  const savedOrder = await newOrder.save();
+  socket.join(roomName);
 
-  // WebSocketを通じて全クライアントに新しい注文を通知
-  wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(savedOrder));
-    }
+  socket.on('disconnect', () => {
+    socket.leave(roomName);
   });
-
-  res.json(savedOrder);
 });
 
-// 既存の注文を更新するエンドポイント
-app.put('/orders/:id', async (req, res) => {
-  const updatedOrder = await Order.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  );
-
-  // WebSocketを通じて全クライアントに注文の更新を通知
-  wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(updatedOrder));
-    }
-  });
-
-  res.json(updatedOrder);
+app.get('/orders/:roomName', (req, res) => {
+  const roomOrders = orders.filter(order => order.roomName === req.params.roomName);
+  res.json(roomOrders);
 });
 
-// 注文のカウントを取得するエンドポイント
-app.get('/orders/count', async (req, res) => {
-  const count = await Order.countDocuments();
-  res.json({ count });
+app.post('/orders', (req, res) => {
+  const newOrder = {
+    _id: Date.now().toString(), // この例ではタイムスタンプをIDとして使用していますが、実際のアプリでは適切なIDを生成するべきです。
+    itemName: req.body.itemName,
+    quantity: req.body.quantity,
+    roomName: req.query.roomName
+  };
+
+  orders.push(newOrder);
+
+  io.to(req.query.roomName).emit('order', newOrder);
+
+  res.json(newOrder);
 });
 
-mongoose.connect('mongodb://localhost:27017/orders', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    const server = app.listen(3001, () => console.log('Server is running on port 3001'));
-    server.on('upgrade', (request, socket, head) => {
-      wss.handleUpgrade(request, socket, head, ws => {
-        wss.emit('connection', ws, request);
-      });
-    });
-  })
-  .catch(err => console.error(err));
+app.put('/orders/:orderId', (req, res) => {
+  const orderIndex = orders.findIndex(order => order._id === req.params.orderId);
 
+  if (orderIndex !== -1) {
+    orders[orderIndex].quantity = req.body.quantity;
+
+    io.to(orders[orderIndex].roomName).emit('order', orders[orderIndex]);
+
+    res.json(orders[orderIndex]);
+  } else {
+    res.status(404).send('Order not found');
+  }
+});
+
+server.listen(3001, () => {
+  console.log('Server is listening on port 3001');
+});
