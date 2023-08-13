c4f67cff025001f1f04987acd7bec92199a1bb6b
ysdko <koki.yoshida110@gmail.com>
2023-08-13 18:58:13 +0900

    add:mongodbにデータを保存

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,57 +12,61 @@ const io = socketIo(server);
 app.use(cors());
 app.use(bodyParser.json());
 
-const orders = []; // この例ではメモリ内に注文データを保存しますが、実際のアプリではデータベースを使用するべきです。
+require('dotenv').config();
+
+// データベースの接続設定
+const dbURL = process.env.DB_URL;
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// スキーマの定義
+const OrderSchema = new mongoose.Schema({
+  itemName: String,
+  quantity: Number,
+  roomName: String
+});
+
+const Order = mongoose.model('Order', OrderSchema);
 
 io.on('connection', (socket) => {
   const roomName = socket.handshake.query.roomName;
 
   socket.join(roomName);
-
   socket.on('disconnect', () => {
     socket.leave(roomName);
   });
 });
 
-app.get('/orders/:roomName', (req, res) => {
-  const roomOrders = orders.filter(order => order.roomName === req.params.roomName);
+app.get('/orders/:roomName', async (req, res) => {
+  const roomOrders = await Order.find({ roomName: req.params.roomName });
   res.json(roomOrders);
 });
 
-app.post('/orders', (req, res) => {
-  const newOrder = {
-    _id: Date.now().toString(), // この例ではタイムスタンプをIDとして使用していますが、実際のアプリでは適切なIDを生成するべきです。
+app.post('/orders', async (req, res) => {
+  const newOrder = new Order({
     itemName: req.body.itemName,
     quantity: req.body.quantity,
     roomName: req.query.roomName
-  };
+  });
 
-  orders.push(newOrder);
+  await newOrder.save();
 
   io.to(req.query.roomName).emit('order', newOrder);
 
   res.json(newOrder);
 });
 
-app.put('/orders/:orderId', (req, res) => {
-  const orderIndex = orders.findIndex(order => order._id === req.params.orderId);
+app.put('/orders/:orderId', async (req, res) => {
+  const updatedOrder = await Order.findByIdAndUpdate(req.params.orderId, { quantity: req.body.quantity }, { new: true });
 
-  if (orderIndex !== -1) {
-    orders[orderIndex].quantity = req.body.quantity;
-
-    io.to(orders[orderIndex].roomName).emit('order', orders[orderIndex]);
-
-    res.json(orders[orderIndex]);
+  if (updatedOrder) {
+    io.to(updatedOrder.roomName).emit('order', updatedOrder);
+    res.json(updatedOrder);
   } else {
     res.status(404).send('Order not found');
   }
 });
 
-app.get('/test', (req, res) => {
-  res.json('test');
-});
-
-port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
